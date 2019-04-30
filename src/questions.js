64ef44ab448d9d9692fa83b3cfc69e301c95f3b8
Un Développeur <muceres@users.noreply.github.com>
2019-04-30 20:50:40 +0200

    refactor(questions): replace push() by spread operator
    the spread operator allows to create a new object with a different
    reference. Which is a better way to approach functional programing

--- src/questions.js
+++ src/questions.js
@@ -37,7 +37,7 @@ export const createEmployeeQuestions = function createEmployeeQuestions(
   number_of_employees
 ) {
   let employeeQuestions = [];
-  let employeeQuestion = {
+  const employeeQuestion = {
     type: "input",
     name: `employee_name_${number_of_employees}`,
     message: `Quelle est le nom de l'employé ${number_of_employees} ?`
@@ -46,7 +46,9 @@ export const createEmployeeQuestions = function createEmployeeQuestions(
   if (number_of_employees > 1) {
     employeeQuestions = createEmployeeQuestions(number_of_employees - 1);
   }
-  employeeQuestions.push(employeeQuestion);
+
+  employeeQuestions = [...employeeQuestions, employeeQuestion];
+
   return employeeQuestions;
 };
 
@@ -54,16 +56,16 @@ export const createTruckQuestions = function createTruckQuestions(
   number_of_trucks
 ) {
   let truckQuestions = [];
-  let truckQuestion = [
-    {
-      type: "input",
-      name: `truck_type_${number_of_trucks}`,
-      message: `Quelle est le type du camion ${number_of_trucks} ?`
-    },
+  const truckQuestion = [
     {
       type: "number",
       name: `truck_volume_${number_of_trucks}`,
       message: `Quelle est le volume en m³ du camion ${number_of_trucks} ?`
+    },
+    {
+      type: "input",
+      name: `truck_type_${number_of_trucks}`,
+      message: `Quelle est le type du camion ${number_of_trucks} ?`
     }
   ];
 
@@ -71,8 +73,7 @@ export const createTruckQuestions = function createTruckQuestions(
     truckQuestions = createTruckQuestions(number_of_trucks - 1);
   }
 
-  truckQuestions.push(truckQuestion.pop());
-  truckQuestions.push(truckQuestion.pop());
+  truckQuestions = [...truckQuestions, ...truckQuestion];
 
   return truckQuestions;
 };
