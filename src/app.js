f096c60491657808c02bca6d87cb73b8bc9d22b1
Un Développeur <muceres@users.noreply.github.com>
2019-05-01 12:53:24 +0200

    fix: catch ctrl c for a proper quitting process

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import { chatbot } from "./chatbot";
 import redisClient from "./database/redis-async";
+import chalk from "chalk";
 
 // load configuraiton
 dotenv.config();
 redisClient.on("error").catch(err => console.log("" + err));
+
+// Before quitting the chatbot
+process.on("SIGINT", () => {
+  console.log(chalk.white.bold("A bientôt !"));
+  redisClient.quit(() => process.exit());
+});
 
 chatbot();
